552dace479c2708fefad2d477d18c6b1ac037606
GoncaloCunhaMendes <a22405389@alunos.ulht.pt>
2025-11-21 15:25:32 +0000

    site diw

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import MagiaDoJSX from "@/public/components/MagiaDo/MagiaDoJSX"
+
+export default function page(){
+  return(
+      <div>
+        <h2>Interfaces Modernos</h2>
+        <p>Bem vindo á minha app em React e Next.js.</p>
+        <MagiaDoJSX />
+      </div>
+      
+  )
+}
---
+++ app/tecnologias/page.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import tecnologias from '@/app/data/tecnologias.json';
+import Image from 'next/image'
+
+export default function page() {
+  return (
+    <div>
+        <h2>Tecnologias Exploradas</h2>
+        <ul>
+            {tecnologias.map((tecnologia, i) => {
+                return <li key={i} style={{ backgroundColor: 'darkblue' }}>
+                    {tecnologia.title} - {tecnologia.description} :  {tecnologia.rating}
+                    <Image
+                    src={"/tecnologias/" + tecnologia.image}
+                    alt="Logotipo do react"
+                    width={200}
+                    height={200}
+                    />
+                </li> 
+            })}
+        </ul>
+    </div>
+  )
+}
